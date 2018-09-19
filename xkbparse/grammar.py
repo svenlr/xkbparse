2d8b3ea3ac50ba49bbafffacaf223e9bd20b840f
Sven Langner <sven.langner@tu-braunschweig.de>
2018-09-19 15:19:53 +0200

    can parse key def sections

--- xkbparse/grammar.py
+++ xkbparse/grammar.py
@@ -1,25 +1,139 @@
-import pypeg2
+import re
+
+import pypeg2 as peg
+
+
+class LevelType:
+    """ A level type, such as "FOUR_LEVEL" or "EIGHT_LEVEL". Matches quotes as well. """
+    grammar = "\"", peg.attr("name", re.compile(r"([A-Z]|_|\+)*")), "\""
 
 
 class KeyCode:
-    grammar = "<", pypeg2.name(), ">"
+    """ A key code such as <UP> or <DOWN> """
+    grammar = "<", peg.attr("name", re.compile(r"([A-Z]|\+|-|[0-9])+")), ">"
+
+    @staticmethod
+    def from_name(name):
+        return peg.parse("<" + name + ">", KeyCode)
 
 
 class KeySym:
-    grammar = pypeg2.name()
+    """ Key symbol such as bracketleft, braceleft, 1, 2, 3, a, b, A, B... """
+    grammar = peg.attr("name", peg.word)
 
 
-class KeySymLevels(pypeg2.List):
-    grammar = "[", pypeg2.optional(pypeg2.maybe_some(KeySym, ","), KeySym), "]"
+class KeySymLevels(peg.List):
+    """ A list of key symbols in brackets """
+    grammar = "[", peg.optional(peg.csl(KeySym)), "]"
+
+    @staticmethod
+    def from_strings(strings: list):
+        levels = KeySymLevels()
+        for s in strings:
+            k = KeySym()
+            k.name = s
+            levels.append(k)
+        return levels
 
 
-class KeyDefSymbols:
-    grammar = "symbols", "[", pypeg2.attr("group", pypeg2.Symbol), "]", "=", pypeg2.attr("levels", KeySymLevels)
+class KeyDefSymbolsGroup:
+    """ inside a key definition inside key_symbols { ... }, this is the part symbols[Group1]=[...]"""
+    grammar = "symbols", "[", ["Group", "group"], peg.attr("__num", re.compile(r"[0-9]+")), "]", \
+              "=", peg.attr("levels", KeySymLevels)
+
+    @property
+    def num(self):
+        return int(getattr(self, "__num"))
+
+    @num.setter
+    def num(self, value):
+        setattr(self, "__num", value)
 
 
-class KeyDef(pypeg2.List):
-    grammar = "key", KeyCode, "{", \
-              pypeg2.optional(("type", "=", "\"", pypeg2.attr("type", pypeg2.Symbol), "\""), ","), \
-              pypeg2.maybe_some(KeyDefSymbols, ","), \
-              KeyDefSymbols, \
-              "}"
+class KeyDefOverlay:
+    """ inside a key definition inside key_symbols { ... }, this is the part overlayX=<...>"""
+    grammar = "overlay", peg.attr("__num", re.compile(r"[0-9]")), "=", peg.attr("key_code", KeyCode)
+
+    @property
+    def num(self):
+        return int(getattr(self, "__num"))
+
+    @num.setter
+    def num(self, value):
+        setattr(self, "__num", value)
+
+
+class KeyDefShort:
+    """ short key definition inside key_symbols { ... }, such as key <A> { [a, A] }"""
+    grammar = "key", peg.blank, peg.attr("key_code", KeyCode), peg.blank, "{", \
+              peg.indent(peg.attr("short_levels", KeySymLevels)), peg.endl, "};"
+
+    def convert(self, group_nums: list, type: LevelType):
+        key_def = KeyDef()
+        key_def.type = type
+        for group_num in group_nums:
+            key_def.set_symbols_group(group_num, self.short_levels)
+        return key_def
+
+
+class KeyDef(peg.List):
+    """ A key definition in the xkb_symbols { ... } section """
+    grammar = "key", peg.blank, peg.attr("key_code", KeyCode), peg.blank, "{", peg.endl, \
+              peg.indent(
+                  peg.optional(("type", "=", peg.attr("type", LevelType)), ","), peg.endl,
+                  [KeyDefSymbolsGroup, KeyDefOverlay],
+                  peg.maybe_some(",", peg.endl, [KeyDefSymbolsGroup, KeyDefOverlay]),
+                  peg.optional(","), peg.endl
+              ), "};"
+
+    def get_overlays(self):
+        overlays = []
+        for child in self:
+            if isinstance(child, KeyDefOverlay):
+                overlays.append(child)
+        return overlays
+
+    def get_overlay_nums(self):
+        map(lambda o: o.num, self.get_overlays())
+
+    def get_overlay_by_num(self, num: int):
+        return [child for child in self if isinstance(child, KeyDefOverlay) and child.num == num][0]
+
+    def remove_overlay_by_num(self, num: int):
+        new_list = [child for child in self if not isinstance(child, KeyDefOverlay) or child.num != num]
+        self.clear()
+        for elem in new_list:
+            self.append(elem)
+
+    def set_overlay(self, num: int, key_code: KeyCode):
+        self.remove_overlay_by_num(num)
+        overlay = KeyDefOverlay()
+        overlay.num = num
+        overlay.key_code = key_code
+        self.append(overlay)
+
+    def get_symbols_groups(self):
+        groups = []
+        for child in self:
+            if isinstance(child, KeyDefSymbolsGroup):
+                groups.append(child)
+        return groups
+
+    def get_symbols_group_nums(self):
+        map(lambda syms: syms.group_num, self.get_symbols_groups())
+
+    def get_symbols_group_by_num(self, num: int):
+        return [child for child in self if isinstance(child, KeyDefSymbolsGroup) and child.num == num][0]
+
+    def remove_symbols_group_by_num(self, num: int):
+        new_list = [child for child in self if not isinstance(child, KeyDefSymbolsGroup) or child.num != num]
+        self.clear()
+        for elem in new_list:
+            self.append(elem)
+
+    def set_symbols_group(self, num: int, levels: KeySymLevels):
+        self.remove_symbols_group_by_num(num)
+        group = KeyDefSymbolsGroup()
+        group.num = num
+        group.levels = levels
+        self.append(group)
--- xkbparse/tests/test_grammar.py
+++ xkbparse/tests/test_grammar.py
@@ -10,6 +10,15 @@ def test_KeyCode():
     assert pypeg2.parse("<AE01>", xkbparse.KeyCode).name == "AE01"
 
 
+def test_KeySym():
+    assert pypeg2.parse("KP_Multiply", xkbparse.KeySym).name == "KP_Multiply"
+    assert pypeg2.parse("XF86ClearGrab", xkbparse.KeySym).name == "XF86ClearGrab"
+
+
+def test_TypeString():
+    assert pypeg2.parse("\"CTRL+ALT\"", xkbparse.LevelType).name == "CTRL+ALT"
+
+
 def test_KeySymLevels():
     with open(os.path.join(TEST_DATA_DIR, "list_AE01")) as f:
         parsed = pypeg2.parse(f.read(), xkbparse.KeySymLevels)
@@ -25,10 +34,32 @@ def test_KeySymLevels():
 
 def test_KeyDefSymbols():
     with open(os.path.join(TEST_DATA_DIR, "symbols_AE01")) as f:
-        parsed = pypeg2.parse(f.read(), xkbparse.KeyDefSymbols)
-        assert parsed.group == "Group1"
+        parsed = pypeg2.parse(f.read(), xkbparse.KeyDefSymbolsGroup)
+        assert parsed.num == 1
         assert parsed.levels[2].name == "onesuperior"
 
-# def test_KeyDef():
-#     with open(os.path.join(TEST_DATA_DIR, "AE01")) as f:
-#         parsed = pypeg2.parse(f.read(), xkbparse.KeyDef)
+
+def test_KeyDef():
+    with open(os.path.join(TEST_DATA_DIR, "AE01")) as f:
+        parsed = pypeg2.parse(f.read(), xkbparse.KeyDef)
+        assert parsed.get_symbols_group_by_num(1).levels[2].name == "onesuperior"
+        parsed.set_overlay(1, xkbparse.KeyCode.from_name("UP"))
+        parsed.set_overlay(2, xkbparse.KeyCode.from_name("LEFT"))
+        parsed.set_overlay(2, xkbparse.KeyCode.from_name("DOWN"))
+        parsed.set_symbols_group(2, xkbparse.KeySymLevels.from_strings(["1"]))
+        parsed.set_symbols_group(2, xkbparse.KeySymLevels.from_strings(["1", "2"]))
+        parsed.remove_symbols_group_by_num(1)
+        parsed.remove_overlay_by_num(1)
+        composed = pypeg2.compose(parsed, xkbparse.KeyDef)
+        # print(composed)
+        reparsed = pypeg2.parse(composed, xkbparse.KeyDef)
+        assert reparsed.type.name == "FOUR_LEVEL"
+        assert reparsed.key_code.name == "AE01"
+        assert reparsed.get_symbols_group_by_num(2).levels[0].name == "1"
+        assert reparsed.get_overlay_by_num(2).key_code.name == "DOWN"
+
+    with open(os.path.join(TEST_DATA_DIR, "KPMU")) as f:
+        parsed = pypeg2.parse(f.read(), xkbparse.KeyDef)
+        assert parsed.type.name == "CTRL+ALT"
+        assert parsed.key_code.name == "KPMU"
+        assert parsed.get_symbols_group_by_num(1).levels[2].name == "KP_Multiply"
